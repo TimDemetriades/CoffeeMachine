a9abd6f0839d306de7798a6b5641da4f3dc2d014
Tim Demetriades <tim.demetriades@gmail.com>
2020-06-18 16:25:07 -0400

    CoffeeMachine.py - grammar fixes

--- CoffeeMachine.py
+++ CoffeeMachine.py
@@ -14,6 +14,7 @@ amount_dict = {}
 def print_amounts():
     """
     Prints current values of supplies
+    return: none
     """
     print(f"""
 The coffe machine has:
@@ -26,7 +27,7 @@ The coffe machine has:
 
 def checker(amount, amount_per):
     """
-    Prints current values of supplies
+    Check how many cups it could make with amount of supplies
     param: amount = current amount
     param: amount_per = amount needed per cup
     return: amount of cups current supply could make
@@ -36,6 +37,7 @@ def checker(amount, amount_per):
 def buy():
     """
     Lets user buy coffee
+    return: none
     """
     global water_amount, milk_amount, beans_amount, cups_amount, money_amount
 
@@ -47,7 +49,7 @@ def buy():
             amount_dict['cups'] = checker(cups_amount, 1)
   
             min_amount = min(amount_dict, key=amount_dict.get)          
-            min_value = amount_dict[min_amount]
+            min_value = amount_dict[min_amount]                 
 
             if min_value == 0:
                 print(f'Sorry, not enough {min_amount}!')
@@ -62,7 +64,7 @@ def buy():
                 amount_dict.clear()
                 break
         elif choice == '2':
-            amount_dict['water'] = checker(water_amount, 350)   # 250 ml water = 1 cup
+            amount_dict['water'] = checker(water_amount, 350)   # 350 ml water = 1 cup
             amount_dict['milk'] = checker(milk_amount, 75)     # 75 ml milk = 1 cup
             amount_dict['beans'] = checker(beans_amount, 20)    # 20 g beans = 1 cup
             amount_dict['cups'] = checker(cups_amount, 1)
@@ -107,19 +109,20 @@ def buy():
         elif choice == 'back':
             break
         else:
-            print("Please enter 1, 2, or 3")
+            print("Please enter 1, 2, 3, or back")
             continue
 
 def fill():
     """
     Lets user add supplies
+    return: none
     """
     global water_amount, milk_amount, beans_amount, cups_amount
 
-    water_amount += int(input('Write how many ml of water do you want to add: '))
-    milk_amount += int(input('Write how many ml of milk do you want to add: '))
-    beans_amount += int(input('Write how many grams of coffee beans do you want to add: '))
-    cups_amount += int(input('Write how many disposable cups of coffee do you want to add: '))
+    water_amount += int(input('Write how many ml of water you want to add: '))
+    milk_amount += int(input('Write how many ml of milk you want to add: '))
+    beans_amount += int(input('Write how many grams of coffee beans you want to add: '))
+    cups_amount += int(input('Write how many disposable cups of coffee you want to add: '))
 
 def take():
     """
